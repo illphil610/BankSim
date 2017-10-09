4c262b5b76ef3b8f999c846ae253487a8dcd7cb0
PhilipCappelli <pcappelli901@gmail.com>
2017-10-09 01:52:34 -0400

    Okay, im seriously not going to touch this anymore...

--- src/edu/temple/cis/c3238/banksim/TransferThread.java
+++ src/edu/temple/cis/c3238/banksim/TransferThread.java
@@ -16,7 +16,7 @@ class TransferThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             int toAccount = (int) (bank.size() * Math.random());
             int amount = (int) (maxAmount * Math.random());
             bank.transfer(fromAccount, toAccount, amount);
